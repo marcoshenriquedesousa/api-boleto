c0644cf18abd0a5a4e9d400480e2350e980f055d
unknown <henriquesantos.uefa@gmail.com>
2022-05-12 20:36:55 -0300

    chore: configura inicialização da api

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import "reflect-metadata";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
+/* eslint-disable new-cap */
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import { Request, Response } from 'express'
+import { Routes } from './routes'
+import config from './config/config'
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
 Routes.forEach(route => {
-    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-        const result = (new (route.controller as any))[route.action](req, res, next);
-        if (result instanceof Promise) {
-            result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+  (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+    const result = (new (route.controller)())[route.action](req, res, next)
+    if (result instanceof Promise) {
+      result.then(d => {
+        if (d && d.status) { res.status(d.status).send(d.message || d.errors) } else if (d && d.file) { res.json(d) }
+      })
+    } else if (result !== null && result !== undefined) {
+      res.json(result)
+    }
+  })
+})
 
-        } else if (result !== null && result !== undefined) {
-            res.json(result);
-        }
-    });
-});
+app.listen(config.port, () => {
+  console.log(`Api inicializada na porta ${config.port}`)
+})
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
 
 export const Routes = [
-    // { method: "get", route: "/users", controller: UserController, action: "all" }
-];
+  // { method: "get", route: "/users", controller: UserController, action: "all" }
+]
