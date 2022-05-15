daf4ebf8e902adfd299572741e643d8271b456eb
unknown <henriquesantos.uefa@gmail.com>
2022-05-15 18:47:39 -0300

    feat: Boleto

---
+++ src/presentation/controllers/boleto.ts
@@ -0,0 +1,29 @@
+import { InvalidParamError, MissingParamError } from '../errors'
+import { badRequest, ok, serverError } from '../helpers/http-helper'
+import { Controller, HttpRequest, HttpRespose } from '../protocols'
+import { isDigit } from '../../utils/utils'
+import { BoletoValidadorController } from './boleto-validador'
+import { GetBoletoController } from './get-boleto'
+
+export class BoletoController implements Controller {
+  handle (httpRequest: HttpRequest): HttpRespose {
+    try {
+      const barCode = httpRequest.params.barCode
+      if (!barCode) {
+        return badRequest(new MissingParamError('barCode'))
+      }
+      if (!isDigit(barCode)) {
+        return badRequest(new InvalidParamError('barCode'))
+      }
+      const isValid = new BoletoValidadorController().isValid(barCode)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('barCode'))
+      }
+      const boleto = new GetBoletoController().get(barCode)
+      return ok(boleto)
+    } catch (error) {
+      console.log(error)
+      return serverError(error)
+    }
+  }
+}
